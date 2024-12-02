9b9f6af0d11c379464df8c3f8419c79606511efe
Florian ten Napel <tennapel.f@gmail.com>
2024-12-02 21:16:29 +0100

    wrote cleaner solution

--- 2024/day2/sol.cpp
+++ 2024/day2/sol.cpp
@@ -1,26 +1,16 @@
 #include <iostream>
-#include <iterator>
+#include <numeric>
 #include <vector>
 #include <sstream>
+#include <algorithm>
 
 using namespace std;
 
-template<class It>
-bool is_safe(It first, It last, bool dampened = true)
+bool is_safe(vector<int> report) // copy needs to happen here
 {
-    auto prev = *first++;
-    for (; first != last; first++) {
-        int diff = *first - prev;  
-        if (diff < 1 || diff > 3) {
-            if (!dampened) {
-                dampened = true;
-                continue;
-            }
-            return false;
-        }
-        prev = *first;
-    }
-    return true;
+    adjacent_difference(begin(report), end(report), begin(report));
+    return all_of(++begin(report), end(report), [](int d) { return d >= 1 && d <= 3; })
+        || all_of(++begin(report), end(report), [](int d) { return d <= -1 && d >= -3; });
 }
 
 int main()
@@ -31,7 +21,7 @@ int main()
     vector<vector<int>> reports;
     string line; 
     int d;
-    while (getline(cin, line) && !line.empty()) {
+    while (getline(cin, line)) {
         vector<int> report;
         stringstream stream(line);
         while (stream >> d)
@@ -41,17 +31,24 @@ int main()
 
     // part 1
     long sum = 0;
-    for (auto& r : reports) {
-        if (is_safe(begin(r), end(r)) || is_safe(rbegin(r), rend(r)))
+    for (const auto& r : reports)
+        if (is_safe(r)) 
             ++sum;
-    }
     cout << sum << '\n';
 
     // part 2
     sum = 0;
     for (auto& r : reports) {
-        if (is_safe(begin(r), end(r), false) || is_safe(rbegin(r), rend(r), false) ||
-            is_safe(++begin(r), end(r))|| is_safe(++rbegin(r), rend(r)))
+        bool found = false;
+        if (is_safe(r))
+            found = true;
+        for (auto it = begin(r); !found && it != end(r); it++) {
+            int tmp = *it;
+            it = r.erase(it);
+            found = is_safe(r);
+            it = r.insert(it, tmp);
+        }
+        if (found)
             ++sum;
     }
     cout << sum << '\n';
