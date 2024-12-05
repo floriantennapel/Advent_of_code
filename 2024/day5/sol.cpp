c565c7a3123aba714aef3bfcfbfd625366cd3fce
Florian ten Napel <tennapel.f@gmail.com>
2024-12-05 18:05:05 +0100

    massive simplification

--- 2024/day5/sol.cpp
+++ 2024/day5/sol.cpp
@@ -2,36 +2,9 @@
 #include <sstream>
 #include <vector>
 #include <bitset>
+#include <algorithm>
 
 using namespace std;
-
-bool is_valid(const vector<bitset<100>>& rules, const vector<int>& update)
-{
-    bitset<100> seen;
-    for (int u : update) {
-        if ((rules[u] & seen).any())
-            return false; 
-        seen[u] = true;
-    }
-    return true;
-}
-
-void sort(const vector<bitset<100>>& rules, vector<int>& update)
-{
-    // A good sorting algorithm would require precomputing a topoligical sort of all rules
-    // Insertion sort was a bit easier to implement, and every update is very short so runtime doesn't really matter
-    bitset<100> added; 
-    added[update[0]] = true;
-    for (int i = 1; i < update.size(); i++) {
-        int current = update[i];
-        bitset<100> before(added);
-        for (int j = i; (rules[current] & before).any(); j--) {
-            swap(update[j], update[j-1]);
-            before[update[j]] = false;
-        }
-        added[current] = true;
-    }
-}
 
 int main()
 {
@@ -60,16 +33,18 @@ int main()
         updates.push_back(move(lv));
     }
 
-    long sum_p1 = 0;
-    long sum_p2 = 0;
-    for (auto& update : updates) {
-        if (is_valid(rules, update))
-            sum_p1 += update[update.size() / 2];
+    auto cmp = [&](int a, int b) {return !rules[b][a];};
+
+    int sum1 = 0;
+    int sum2 = 0;
+    for (auto& u : updates) {
+        if (ranges::is_sorted(u, cmp))
+            sum1 += u[u.size() / 2];
         else {
-            sort(rules, update);
-            sum_p2 += update[update.size() / 2];
+            ranges::sort(u, cmp);
+            sum2 += u[u.size() / 2];
         }
     }
-    cout << sum_p1 << '\n';
-    cout << sum_p2 << '\n';
+    cout << sum1 << '\n';
+    cout << sum2 << '\n';
 }
