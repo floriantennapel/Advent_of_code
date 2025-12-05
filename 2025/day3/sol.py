fb07e2147054c12dfd5a44382cf2c2e4e42a31b1
Florian ten Napel <tennapel.f@gmail.com>
2025-12-05 15:59:46 +0100

    simplified to not use dp

--- 2025/day3/sol.py
+++ 2025/day3/sol.py
@@ -1,20 +1,15 @@
 from sys import stdin
 
 def jolt(bat, s):
-    dp = [[-1]*len(bat) for _ in range(s)]
-    sup = -1
-    for j in range(len(bat)-1, -1, -1):
-        sup = max(sup, bat[j])
-        dp[0][j] = sup
-
-    pow = 10
-    for i in range(1, s):
-        for j in range(len(bat)-1-i, -1, -1):
-            sup = max(sup, bat[j]*pow + dp[i-1][j+1])
-            dp[i][j] = sup
-        pow *= 10
-
-    return dp[s-1][0]
+    bat = list(enumerate(bat))
+    a = 0
+    total = 0
+    for i in reversed(range(s)):
+        ran = bat[a:-i] if i != 0 else bat[a:]
+        ind, biggest = max(ran, key=lambda p: p[1])
+        total = total * 10 + biggest
+        a = ind+1
+    return total
 
 if __name__ == '__main__':
     inp = [list(map(int, line.strip())) for line in stdin]
