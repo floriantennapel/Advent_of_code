09c2954df6dcb293d5ed434c90427214fe5f3f7e
Florian ten Napel <tennapel.f@gmail.com>
2025-12-19 11:17:49 +0100

    learned about linear optimization

--- 2025/day10/sol.py
+++ 2025/day10/sol.py
@@ -1,8 +1,8 @@
 from queue import Queue
 from sys import stdin
-import sympy as sp
-from itertools import product
-from tqdm import tqdm
+import scipy.optimize as opt
+import scipy as sp
+import numpy as np
 
 def button_int(button):
     res = 0
@@ -10,7 +10,7 @@ def button_int(button):
         res |= (1 << b)
     return res
 
-def diag_int(diag):
+def diagram_int(diag):
     res = 0
     for i, c in enumerate(diag):
         if c == '#':
@@ -41,47 +41,38 @@ def _p1(target, buttons):
     return -1
 
 def _p2(buttons, jolts):
-    max_range = max(jolts)
+    '''
+    As this is a integer linear optimization problem, I am using the scipy.optimize.linprog
+    function to solve for optimal coefficients.
+
+    After having watched some lectures on linear programming, I realize it is way too much work to
+    implement this from scratch. 
+    My original solution found unknown coefficients using gaussian elimination and iterated over all 
+    possible values to find a minimum. This was kind of slow and also relied on external libraries.
+
+    If I was unable to use external libraries I would probably write the gaussian elimination algorithm
+    from scratch and be a bit smarter about choosing the unknowns.
+    '''
     m = len(jolts)
     n = len(buttons)
-    A = sp.zeros(m, n)
-    for i, b in enumerate(buttons):
-        for j in b:
-            A[j, i] = 1
+    A = np.zeros((m,n))
+    c = np.ones(n) # minimizing for linear sum of coefficients
+    b = np.array(jolts) # setting as both lower and upper bound i.e. equality bound
 
-    b = sp.Matrix(jolts)
-    sol = list(sp.linsolve((A, b)))[0]
-    syms = set()
-    for s in sol:
-        for sym in s.free_symbols:
-            syms.add(sym)
-    syms = list(syms)
+    for j, but in enumerate(buttons):
+        for i in but:
+            A[i, j] = 1
 
-    res = sum(sol)
-    if len(syms) == 0:
-        return int(res)
-
-    # too lazy to think of a smart way to do this, 
-    # brute forcing the rest should work
-    lo = 1<<31
-    for p in product(range(max_range+1), repeat=len(syms)):
-        interp = dict(zip(syms, p)) 
-        subs_sum = 0
-        for s in sol:
-            res = s.subs(interp)
-            if res < 0 or not res.is_integer:
-                break
-            subs_sum += res
-        else:
-            lo = min(lo, int(subs_sum)) #TODO
-    return lo
+    integrality = np.ones_like(c)
+    res = opt.linprog(c=c, A_eq=A, b_eq=b, integrality=integrality)
+    return int(res.fun)
 
 p1 = 0
 p2 = 0
 lines = stdin.readlines()
-for line in tqdm(lines):
+for line in lines:
     splitted = line.strip().split()
-    target = diag_int(splitted[0][1:-1])
+    target = diagram_int(splitted[0][1:-1])
     buttons = []
     for s in splitted[1:]:
         if s[0] == '{':
